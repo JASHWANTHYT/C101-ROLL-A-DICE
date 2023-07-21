f7ec9285e3cbadc79f65ce7372952e7dcd6e2023
JASHWANTHYT <86235521+JASHWANTHYT@users.noreply.github.com>
2023-07-21 19:06:33 +0530

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,46 @@
+import random
+
+response = "y"
+
+while response == "y":
+    no = random.randint(1, 6)
+    if no == 1:
+        
+        print("      ")
+        print("  *   ")
+        print("      ")
+       
+    elif no == 2:
+       
+        print(" *    ")
+        print("      ")
+        print("    * ")
+       
+    elif no == 3:
+       
+        print(" *     ")
+        print("   *   ")
+        print("     * ")
+      
+    elif no == 4:
+        
+        print(" *   * ")
+        print("       ")
+        print(" *   * ")
+        
+        
+    elif no == 5:
+        
+        print(" *   * ")
+        print("   *   ")
+        print(" *   * ")
+       
+    elif no == 6:
+      
+        print(" *   * ")
+        print(" *   * ")
+        print(" *   * ")
+    
+    response = input("Do you want to roll the dice again? (y/n): ").lower()
+
+print("Thanks for playing! Goodbye!")
